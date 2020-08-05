6338f43921ac432a3f7586f090551d38e6ce0063
Joshua Corpin <jkcorpin@gmail.com>
2020-08-05 20:39:01 +0800

    Added click handler for every country

--- src/components/UI/Table/Table.js
+++ src/components/UI/Table/Table.js
@@ -6,14 +6,21 @@ const Table = ({ countries, label }) => {
     return number.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
   };
 
+  const countryClickHandler = () => {
+    window.scrollTo(0, 0);
+  };
+
   const topCountries = countries
-    .filter((c) => c.countryCode && c.countryCode !== "OT")
+    .filter(
+      (c) => c.countryCode && c.countryCode !== "OT" && c.totalConfirmed !== 0
+    )
     .map((country, i) => {
       return (
         <tr key={i}>
           <td>
             <Link
               to={`/affected-countries/${country.countryCode.toLowerCase()}`}
+              onClick={countryClickHandler}
             >
               <i
                 className={`pr-5 flag-icon flag-icon-${country.countryCode.toLowerCase()}`}
@@ -34,13 +41,13 @@ const Table = ({ countries, label }) => {
 
   return (
     <div className="col-12 my-2">
-      <div className={countries.length > 10 ? "tableHeight" : null}>
+      <div className="tableFixHead tableHeight">
         <table className="table">
           <thead>
             <tr className="text-center">
               <th colSpan="4">{label}</th>
             </tr>
-            <tr className="clickable-row">
+            <tr>
               <th scope="col">Countries</th>
               <th scope="col">Confirmed</th>
               <th scope="col">Recovered</th>
