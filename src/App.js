4d42bf4bfb35ee68934f3486a674824f051faa65
Joshua Corpin <jkcorpin@gmail.com>
2020-08-05 20:32:49 +0800

    Added Route for countries

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
-import Affected from "./cotainers/Affected/Affected";
 import Home from "./cotainers/Home/Home";
+import Affected from "./cotainers/Affected/Affected";
+import Country from "./cotainers/Country/Country";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -11,6 +12,7 @@ const App = (props) => {
     <div className="container">
       <Layout>
         <Switch>
+          <Route path="/affected-countries/:cc" component={Country} />
           <Route path="/affected-countries" component={Affected} />
           <Route path="/" component={Home} />
         </Switch>
