f447e31c1663c1bd7a8ec018d06cd4cad3e80886
Joshua Corpin <jkcorpin@gmail.com>
2020-08-06 16:15:12 +0800

    exportable components

---
+++ src/components/UI/index.js
@@ -0,0 +1,6 @@
+export { default as ColumnChart } from "./Chart/ColumnChart/ColumnChart";
+export { default as LineChart } from "./Chart/LineChart/LineChart";
+export { default as Navigation } from "./Navigation/Navigation";
+export { default as Spinner } from "./Spinner/Spinner";
+export { default as Table } from "./Table/Table";
+export { default as Tabular } from "./Tabular/Tabular";
